5ac107aa5b9137db7a0f7b67e8224491ea3e79a1
KStrelskey <strelskiy37@gmail.com>
2023-05-31 19:12:59 +0300

    js/scroll

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Анимация текса (печатная машинка)
 let app = document.querySelector(".body-home__profession");
 let typewriter = new Typewriter(app, {
     loop: true,
@@ -12,8 +13,43 @@ typewriter
     .typeString("HTML Developer")
     .pauseFor(300)
     .start();
+// Анимация текса (печатная машинка)
 
-const itemsBtn = document.querySelectorAll(".block-experience__btn");
-const itemsBody = document.querySelectorAll(".block-experience__item-body");
+// Анимирования при скролле
+const animItems = document.querySelectorAll("._anim-items"); // получаем все анимируемые элементы
+if (animItems.length > 0) {
+    window.addEventListener("scroll", animOnScrool);
+    function animOnScrool() {
+        for (let index = 0; index < animItems.length; index++) {
+            const animItem = animItems[index]; // перебираем их
+            const animItemHeight = animItem.offsetHeight; // высота блок анимируемого элемента
+            const animItemOffset = offset(animItem); // сумма высота в видимой части и сколько проскроллили
+            let animItemPoint = window.innerHeight - animItemHeight / 5; // точка анимирования элемента; window.innerHeight - видимая область всего окна пользователя
+            if (animItemHeight > window.innerHeight) {
+                animItemPoint = window.innerHeight - window.innerHeight / 5;
+            } // если высота блока элемента больше высоты области видимости экрана пользователя
+            if (
+                pageYOffset > animItemOffset - animItemPoint &&
+                pageYOffset < animItemOffset + animItemPoint
+            ) {
+                animItem.classList.add("_active");
+            } else {
+                animItem.classList.remove("_active");
+            } // момент, когда добавляется или удаляется класс
+            // pageYOffset - количество пикселей, прокручиваемых по вертикальной оси (вниз и вверх)
+        }
+    }
+    function offset(el) {
+        const rect = el.getBoundingClientRect(); // getBoundingClientRec - обнаруживает размер элемента и его обнаружение относительно области просмотра
+        //(часть страницы, видимая на экране, которую мы видим)
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return rect.top + scrollTop; // rect.top - высота до объекта в видимой части страницы
+        // scrollTop - сколько проскроллили
+    }
 
-
+    setTimeout(() => {
+        animOnScrool();
+    }, 300); // вызывает функцию анимирования сразу же как загрузилась
+    // страницы через 300 миллисекунд
+}
+// Анимирования при скролле
